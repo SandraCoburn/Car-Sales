ef8df5436f76ec7ff85e7ec9f199bb4b7290fae5
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-26 21:21:06 -0700

    Saved changes

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { removeFeature } from "../actions/feauturesActions";
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
@@ -12,13 +12,13 @@ const AddedFeature = props => {
       >
         X
       </button>
-      {props.feature.name}
+      {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    name: state.features
+    name: state.features,
   };
 };
 export default connect(mapStateToProps, { removeFeature })(AddedFeature);
